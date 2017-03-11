6ae3273ca4bbfaef52ba4e95df5c657d86e214ac
Rafael Souza da Silva <computacao.rafael@gmail.com>
2017-03-11 02:06:16 -0300

    Aula 76: IndexRoutes com React Router

---
+++ src/components/posts_index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <div>Lista de Posts do Blog</div>
+}
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,14 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
-
-const Greeting =() => {
-  return <div>Hey There!</div>;
-};
+import PostsIndex from './components/posts_index';
 
 //Mapeando routes para componentes
 export default ( //Sempre que uma route é aninhada, o componente pai tem de
-  //renderizar um this.props.children
+  //renderizar um this.props.children.
+  //Uma IndexRoute funciona como se fosse uma route aninhada com
+  //o mesmo path da route pai
   <Route path="/" component={App}>
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
+    <IndexRoute component={PostsIndex} />
   </Route>
 );
