c2e7c790aca6b8251b260ba8fbb9249f3c8e0d95
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-03-13 12:53:23 -0300

    Aula 81: Buscando os dados em um método do ciclo de vida do componente. Utilizando um atalho do react-redux para não precisar do mapDispatchToProps

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
 
 class PostsIndex extends Component {
   //Método do ciclo de vida do componente,
   //o nome é definido pelo React
   componentWillMount() {
-    console.log('chamar o action creator aqui para buscar os posts');
+    this.props.fetchPosts();
   }
 
   render() {
@@ -13,5 +15,7 @@ class PostsIndex extends Component {
     );
   }
 }
-
-export default PostsIndex;
+//Um atalho para não precisar usar a mapDispatchToProps
+//junto com a sintaxe ES6 pra atribuição de variáveis
+//com mesmo nome fetchPosts
+export default connect(null, {fetchPosts})(PostsIndex);
