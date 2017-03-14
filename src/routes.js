10f3d19a50fa9060b1f1728f491b24052cfe555d
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-03-14 12:09:48 -0300

    Aula 82: Criando o form para novos posts

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 //Mapeando routes para componentes
 export default ( //Sempre que uma route é aninhada, o componente pai tem de
@@ -11,5 +12,6 @@ export default ( //Sempre que uma route é aninhada, o componente pai tem de
   //o mesmo path da route pai
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={PostsNew} />
   </Route>
 );
