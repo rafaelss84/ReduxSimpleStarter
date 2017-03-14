521faf03671099a1b4a03bcac74e9b51d7728db5
Rafael Souza da Silva <computacao.rafael@gmail.com>
2017-03-14 19:52:28 -0300

    Aula 88: Validação do form - parte 1

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -13,6 +13,9 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Título</label>
           <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
@@ -31,13 +34,25 @@ class PostsNew extends Component {
   }
 }
 
+// A função é chamada sempre que o form é alterado, e previne que o form seja enviado
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Informe um título'; //o nome da propriedade deve ser o nome do campo
+  }
+
+  return errors;
+}
+
 //Aqui é passada a configuração do form para o ReduxForm, tem o mesmo comportamento
 //do connect do React.
 //connect: primeiro argumento é o mapStateToProps, segundo é o mapDispatchToProps
 //redurForm: primeiro é configuração do form, depois os do connect
 export default reduxForm({
   form: 'PostsNewForm', //não precisa ser o mesmo nome do componente, mas tem de ser único
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate //função declarada para validação
 }, null, {createPost})(PostsNew);
 
 //Anotação apenas para registro. O que o ReduxForm faz é criar a seguinte
