baa90223098fa00201c7596de59488a95a58e1b2
Rafael Souza da Silva <computacao.rafael@gmail.com>
2017-03-14 19:27:21 -0300

    Aula 86: Passando o controle para o Redux Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,23 +3,25 @@ import {reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    const {fields: {title, categories, content}, handleSubmit} = this.props; //Sintaxe ES6
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Criar novo Post</h3>
-        
+
         <div className="form-group">
           <label>Título</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title}/>
         </div>
 
         <div className="form-group">
           <label>Categorias</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories}/>
         </div>
 
         <div className="form-group">
           <label>Conteúdo</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content}/>
         </div>
 
         <button type="submit" className="btn btn-primary">Criar</button>
@@ -30,7 +32,7 @@ class PostsNew extends Component {
 
 //Aqui é passada a configuração do form para o ReduxForm
 export default reduxForm({
-  form: 'PostsNew', //não precisa ser o mesmo nome do componente, mas tem de ser único
+  form: 'PostsNewForm', //não precisa ser o mesmo nome do componente, mas tem de ser único
   fields: ['title', 'categories', 'content']
 })(PostsNew);
 
